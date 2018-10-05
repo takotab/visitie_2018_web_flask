d72b4500655b96688adcdc292fbc8aae190482d5
Tako Tabak <tako.tabak@imec-nl.nl>
2018-10-05 15:45:16 +0200

    I can now ask multiple questions

--- visitatie/formulieren/froms.py
+++ visitatie/formulieren/froms.py
@@ -4,7 +4,6 @@ from wtforms.validators import ValidationError, Email, EqualTo, DataRequired
 
 
 class GegevensCheck(FlaskForm):
-    # TODO add wachtwoord bezoekde praktijk
     ww_bezoekende = PasswordField("Wachtwoord bezoekende Praktijk")
     ww_current_user = PasswordField("Uw Wachtwoord")
     alles_klopt = BooleanField('Alles klopt')
--- visitatie/formulieren/routes.py
+++ visitatie/formulieren/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, redirect, render_template, request, url_for, flash
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
+import json
 
 from visitatie import db
 from visitatie.data_models import User
@@ -32,3 +33,19 @@ def init_form():
                            title = 'Gegevens check',
                            form = form,
                            user = current_user)
+
+
+@bp.route("/form_praktijk_vragen", methods = ['GET', 'POST'])
+@login_required
+def form_praktijk_vragen():
+    for key in request.args.keys():
+        print(key, request.args.get(key))
+
+    your_questions = []
+    with open('praktijkvragen.txt', 'r', encoding = 'UTF-8') as f:
+        for i, line in enumerate(f):
+            # print(line.replace('\n', ''))
+            your_questions.append((str(i), line.replace('\n', '')))
+    return render_template("form_praktijk_vragen.html",
+                           your_questions = your_questions,
+                           title = "Praktijk vragen")
